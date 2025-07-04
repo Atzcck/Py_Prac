73845a6825ae79a9a590182163cd221a09b163e1
Atzcck <alptazecicek@gmail.com>
2025-07-04 17:53:00 +0300

    Add initial matplotlib plotting script with example data points

---
+++ matplotlibPrac.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#print(matplotlib.__version__)
+
+#ypoints = np.array([0, 250])
+#xpoints = np.array([0, 6])
+
+#plt.plot(xpoints, ypoints)
+#plt.show()
+
+
+apoints = np.array([3, 8, 1, 10])
+
+plt.plot(apoints, 'o:r')
+plt.show()
