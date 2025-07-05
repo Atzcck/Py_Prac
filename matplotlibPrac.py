bdc4e9d6fb5635421251894a24222fddee523004
Atzcck <alptazecicek@gmail.com>
2025-07-05 10:38:22 +0300

    Refactor matplotlibPrac.py to enable plotting and add subplot.py for multiple plots demonstration

--- matplotlibPrac.py
+++ matplotlibPrac.py
@@ -3,14 +3,14 @@ import numpy as np
 
 #print(matplotlib.__version__)
 
-#ypoints = np.array([0, 250])
-#xpoints = np.array([0, 6])
+ypoints = np.array([0, 250, 2])
+#xpoints = np.array([0, 6, 10])
 
-#plt.plot(xpoints, ypoints)
-#plt.show()
-
-
-apoints = np.array([3, 8, 1, 10])
-
-plt.plot(apoints, 'o:r')
+plt.plot(ypoints)
 plt.show()
+
+
+#apoints = np.array([3, 8, 1, 10])
+
+#plt.plot(apoints, 'o:r')
+#plt.show()
